5e3a25b05fd07abe8941a0cd58554ddfb8c63325
norovsuren15 <e.norvoo@gmail.com>
2025-12-15 12:42:35 +0800

    fs file & filder

--- fs/index.js
+++ fs/index.js
@@ -1,32 +1,57 @@
-const { error } = require("console");
 const fs = require("fs");
 
-let data = "Сайн байна уу, Node.js!";
+// let data = "Сайн байна уу, Node.js!";
 
-fs.writeFile("./message.txt", data, (error) => {
+// fs.writeFile("./message.txt", data, (error) => {
+//   if (error) {
+//     console.log("Aldaa garlaa");
+//   } else {
+//     console.log("File amjilttai uusgegdlee");
+//   }
+// });
+
+// fs.readFile("./message.txt", { encoding: "utf-8" }, (err, data) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   } else {
+//     console.log(data, "File read successfully!");
+//     console.log(data);
+//   }
+// });
+
+// data = "Өнөөдөр сайхан өдөр.";
+
+// fs.appendFile("./message.txt", data, (error) => {
+//   if (error) {
+//     console.log("Aldaa garlaa");
+//   } else {
+//     console.log("text amjilttai uusgegdlee");
+//   }
+// });
+
+// fs.unlink("./message.txt", (error) => {
+//   if (error) {
+//     console.log("aldaa garlaa");
+//   } else {
+//     console.log("File amjilttai ustgagdlaa.");
+//   }
+// });
+
+fs.mkdir("./data", (error) => {
   if (error) {
-    console.log("Aldaa garlaa");
+    console.log("aldaa garlaa");
   } else {
-    console.log("File amjilttai uusgegdlee");
+    console.log("Folder amjilttai uusgelee.");
   }
 });
 
-data = "Өнөөдөр сайхан өдөр.";
+const data = "Ene bol ugugdul";
 
-fs.appendFile("./message.txt", data, (error) => {
+fs.writeFile("./data/info.txt", data, (error) => {
   if (error) {
-    console.log("Aldaa garlaa");
+    console.log("aldaa garlaa");
   } else {
-    console.log("text amjilttai uusgegdlee");
-  }
-});
-
-fs.readFile("./message.txt", { encoding: "utf-8" }, (err, data) => {
-  if (err) {
-    console.log(err);
-    return;
-  } else {
-    console.log(data, "File read successfully!");
-    console.log(data);
+    console.log("File amjilttai uusgelee.");
   }
 });
